2cc163f7eba3ecfb4ee3579b56f5c8251c85f95b
hyrvx <152061313+hyrvx@users.noreply.github.com>
2025-01-18 15:02:12 +0100

    Add Settings Menu

--- main.ts
+++ main.ts
@@ -1,29 +1,75 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 import Katakana_Characters from 'Katakana.json'
 
+interface KatakanaPluginSettings {
+  toggleQuotations: boolean;
+}
+
+const DEFAULT_SETTINGS: Partial<KatakanaPluginSettings> = {
+  toggleQuotations: true,
+}
+
 const Katakana = Katakana_Characters.katakana;
 export default class KatakanaPlugin extends Plugin {
+  settings: KatakanaPluginSettings;
+  
+  // Load / Save Settings
+  async loadSettings() { this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData()); }
+  async saveSettings() { await this.saveData(this.settings) };
+  
+
   async onload() {
+
 	console.log("Katakana Plugin loaded!");
+
+  // Create Settings Tab
+  this.addSettingTab(new KatakanaPluginSettings(this.app, this));
 
   // Create new Statusbar Item
   let katakana_item = this.addStatusBarItem();
   displayNewKatakana(katakana_item);
   
   this.addCommand({
-    id: "load-new-katakana",
-    name: "Load new Katakana",
-    callback: () => {
-      displayNewKatakana(katakana_item);
+      id: "load-new-katakana",
+      name: "Load new Katakana",
+      callback: () => {
+        displayNewKatakana(katakana_item);
+      }
     }
-  }
-
   )
 	
   }
   async onunload() {
     // Release any resources configured by the plugin.
 	console.log("Katakana Plugin unloaded!");
+  }
+}
+
+class KatakanaPluginSettings extends PluginSettingTab
+{
+  plugin: KatakanaPlugin;
+
+  constructor(app: App, plugin: KatakanaPlugin) {
+    super(app, plugin);
+    plugin = this.plugin;
+  }
+
+  display(): void {
+    let { containerEl } = this;
+
+    containerEl.empty();
+
+    new Setting(containerEl)
+    .setName("Display Quotations")
+    .setDesc("Enable japanese style quotations")
+    .addToggle((toggle) =>
+      toggle
+      .setValue(this.plugin.settings.toggleQuotations)
+      .onChange(async (value) => {
+        this.plugin.settings.toggleQuotations = value;
+        await this.plugin.saveSettings();
+      })
+    )
   }
 }
 
